ca700436cb6b600263e445c42676910dee16deae
canq <lingz@princetechs.com>
2016-04-28 17:49:54 +0800

    mf

--- clawer/collector/utils_cron.py
+++ clawer/collector/utils_cron.py
@@ -1,4 +1,4 @@
-        #
+                                                                                                                                                                                                                                    #
 # Copyright 2015, Martin Owens <doctormo@gmail.com>
 #
 # This library is free software; you can redistribute it and/or
