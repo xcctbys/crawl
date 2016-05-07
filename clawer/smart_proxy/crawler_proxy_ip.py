2dbbc71155655ed19fa2b4d9e62dad41c76b6efe
haijunt <haijunt@princetechs.com>
2016-05-07 22:07:21 +0800

    haijunt

--- clawer/smart_proxy/crawler_proxy_ip.py
+++ clawer/smart_proxy/crawler_proxy_ip.py
@@ -9,35 +9,35 @@ from models import ProxyIp
 
 proxy_list = [XiciProxy, HaoProxy, KuaiProxy, IPCNProxy, SixProxy, YouProxy]
 
-class Crawer(object):
-    def __init__(self):
-        self.proxyip = None
-        pass
+class Crawler(object):
+	def __init__(self):
+		self.proxyip = None
+		pass
 
-    def do_with(self, func):
-        #每个插件返回的是代理IP地址加端口号的列表
-        try:
-            address = func.run()
-            if not address:
-                address = []
-            for item in address:
-                print item
-                try:
-                    self.proxyip = ProxyIp(ip_port=item[0], province=item[1], is_valid=False) #放入mysql中
-                    self.proxyip.save()
-                except:
-                    pass
-        except Exception as e:
-            # print 'error in do_with',e
-            pass
+	def do_with(self, func):
+		#每个插件返回的是代理IP地址加端口号的列表
+		try:
+			address = func.run()
+			if not address:
+				address = []
+			for item in address:
+				print item
+				try:
+					self.proxyip = ProxyIp(ip_port=item[0], province=item[1], is_valid=False) #放入mysql中
+					self.proxyip.save()
+				except:
+					pass
+		except Exception as e:
+			# print 'error in do_with',e
+			pass
 
-    def run(self):
-        #得到有多少个爬虫，多少个插件。
-        for item in proxy_list:
-            # print 'item:----', item
-            self.do_with(item())
+	def run(self):
+		#得到有多少个爬虫，多少个插件。
+		for item in proxy_list:
+			# print 'item:----', item
+			self.do_with(item())
 if __name__ == '__main__':
-    os.environ["DJANGO_SETTINGS_MODULE"] = "crawer.settings"
-    django.setup()
-    crawer = Crawer()
-    crawer.run()
+	os.environ["DJANGO_SETTINGS_MODULE"] = "crawler.settings"
+	django.setup()
+	crawler = Crawler()
+	crawler.run()
--- clawer/smart_proxy/management/commands/crawlerproxyip.py
+++ clawer/smart_proxy/management/commands/crawlerproxyip.py
@@ -8,6 +8,6 @@ from django.core.management.base import BaseCommand
 from smart_proxy.crawler_proxy_ip import Crawler
 
 class Command(BaseCommand):
-    def handle(self, *args, **options):         
-        crawler = Crawler()
-        crawler.run()
+	def handle(self, *args, **options):         
+		crawler = Crawler()
+		crawler.run()
