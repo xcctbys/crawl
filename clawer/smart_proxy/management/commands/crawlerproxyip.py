ea2e768d65aeb2f1fff11425240668882135e335
haijunt <haijunt@princetechs.com>
2016-05-07 21:56:23 +0800

    haijunt

---
+++ clawer/smart_proxy/management/commands/crawlerproxyip.py
@@ -0,0 +1,11 @@
+#coding:utf8
+import sys
+# sys.path.append('/Users/princetechs3/Documents/pyenv/cr-clawer/clawer/')
+
+from django.core.management.base import BaseCommand
+from smart_proxy.crawler_proxy_ip import Crawler
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):         
+        crawler = Crawler()
+        crawler.run()
