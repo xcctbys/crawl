44c3ea8901205d2ff62ff2966be9414a816e957e
canq <lingz@princetechs.com>
2016-05-09 09:44:16 +0800

    save.sh

---
+++ confs/cr/filter_get.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+WORKDIR="/home/clawer/cr-clawer/clawer"
+PYTHON="/usr/local/bin/python"
+LOCALDIR="/Users/princetechs/cr-clawer"
+
+
+/usr/local/bin/mongofiles  get ${LOCALDIR}/dump.rdb
