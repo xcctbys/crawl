14013af0b77881839378f40850ead5ed6aa26d8e
haijunt <haijunt@princetechs.com>
2016-05-15 19:57:17 +0800

    haijunt

--- clawer/enterprise/utils.py
+++ clawer/enterprise/utils.py
@@ -22,7 +22,8 @@ from .libs.shaanxi_crawler import ShaanxiCrawler
 ##
 # from .libs.shanghai_crawler import ShanghaiCrawler
 from .libs.tt_shanghai_crawler import ShanghaiCrawler
-from .libs.zongju_crawler import ZongjuCrawler
+# from .libs.zongju_crawler import ZongjuCrawler
+from .libs.tt_zongju_crawler import ZongjuCrawler
 # from .libs.jiangsu_crawler import JiangsuCrawler
 from .libs.test_jiangsu_crawler import JiangsuCrawler
 from .libs.heilongjiang_crawler import HeilongjiangClawer
