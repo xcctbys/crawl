7ecbde4c02182787e09f4067c5ca01796fcb88c2
zhongyid <zhongyid@princetechs.com>
2016-05-16 13:35:48 +0800

    deploy.

--- clawer/manage_cr.py
+++ clawer/manage_cr.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "clawer.settings_cr")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "clawer.settings.cr")
 
     from django.core.management import execute_from_command_line
 
