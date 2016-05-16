29b9a8774aa2f55e3586be33a68c087f80e892a5
zhengful <zhengful@princetechs.com>
2016-05-16 18:37:19 +0800

    Structure Update

--- clawer/structure/models.py
+++ clawer/structure/models.py
@@ -19,16 +19,11 @@ class Parser(Document):
 
 class StructureConfig(Document):
      job = ReferenceField(Job)
-     crawlertask = ReferenceField(CrawlerTask)
      parser = ReferenceField(Parser)
      db_xml = StringField()
      
 class CrawlerAnalyzedData(Document):
-     uri = StringField(max_length =8000)
-     job = ReferenceField(Job)
+     crawler_task = ReferenceField(CrawlerTask)
      update_date = DateTimeField(default=datetime.datetime.now())
      analyzed_data = StringField()
-
-class ParseJobInfo(Document):
-     rq_parse_job_id = StringField()#把生成解析任务时的RQ job.id结构化，这样job失败一定次数后利用触发函数在数据库中找到StructureConfig中的CrawlerTask并改变其标志位为解析失败
-     crawlerdownloaddata = ReferenceField(CrawlerDownloadData)  
+     retry_times = IntField(default = 0)
