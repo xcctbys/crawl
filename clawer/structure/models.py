d4921ed823647469da00a2c9d25cec5dfaf61bcf
zhengful <zhengful@princetechs.com>
2016-05-24 09:55:00 +0800

    Structure Update

--- clawer/structure/models.py
+++ clawer/structure/models.py
@@ -4,7 +4,8 @@ import json
 import datetime
 from django.db import models
 #from storage.models import Job
-from storage.models import Job
+from storage.models import Job as JobMySQL
+from collector.models import Job as JobMongoDB
 from collector.models import CrawlerTask, CrawlerDownloadData
 from mongoengine import (Document,
                               IntField,
@@ -24,7 +25,7 @@ class Parser(models.Model):
 class StructureConfig(models.Model):
      job_copy_id = models.CharField(unique = True, max_length = 100)
      job = models.OneToOneField(
-          Job,
+          JobMySQL,
           on_delete = models.CASCADE)
 
      parser = models.OneToOneField(
@@ -48,8 +49,8 @@ class Extracter(Document):
     extracter_config = StringField()
 
 class ExtracterStructureConfig(Document):
-    job = ReferenceField(Job)
-    extracter = ReferenceField(Extracter))
+    job = ReferenceField(JobMongoDB)
+    extracter = ReferenceField(Extracter)
 
 class CrawlerExtracterInfo(Document):
     extract_task = ReferenceField(CrawlerTask)
