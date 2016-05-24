7a2e02fbd7794151b4b7fce4e6c9d0a6a1424790
zhongyidai <zhongyid@princetechs.com>
2016-05-24 16:16:03 +0800

    Add migrations.

---
+++ clawer/structure/migrations/0001_initial.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('storage', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Parser',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('parser_id', models.CharField(unique=True, max_length=100)),
+                ('python_script', models.TextField()),
+                ('update_date', models.DateField(verbose_name=datetime.datetime(2016, 5, 24, 16, 12, 46, 742566))),
+            ],
+        ),
+        migrations.CreateModel(
+            name='StructureConfig',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('job_copy_id', models.CharField(unique=True, max_length=100)),
+                ('update_date', models.DateField(verbose_name=datetime.datetime(2016, 5, 24, 16, 12, 46, 743195))),
+                ('job', models.OneToOneField(to='storage.Job')),
+                ('parser', models.OneToOneField(to='structure.Parser')),
+            ],
+        ),
+    ]
