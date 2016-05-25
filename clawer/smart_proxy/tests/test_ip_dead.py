3ad7ece479781f9a1a656334a378ee33f1485aee
canq <lingz@princetechs.com>
2016-05-25 14:48:10 +0800

    .

--- clawer/smart_proxy/tests/test_ip_dead.py
+++ clawer/smart_proxy/tests/test_ip_dead.py
@@ -13,10 +13,10 @@ def test_ip_dead(nums,uri_list):
         print i
         for uri in uri_list:
             print uri
-            rest = reqst.get(uri,timeout=300)
+            rest = reqst.get(uri,timeout=60)
             print rest.content
             print rest
-            time.sleep(60)
+            #time.sleep(60)
             print i
 
 
@@ -25,6 +25,8 @@ if __name__ == "__main__":
     #print proxy
     nums= 10000
     #211.94.187.236   工商主页
+    uri22= 'http://gsxt.gdgs.gov.cn/aiccips/CheckEntContext/showInfo.html'
+    uri23='http://gsxt.gdgs.gov.cn'
     uri = 'http://www.baidu.com'
     uri1 = 'http://qyxy.baic.gov.cn/'
     uri2 = 'http://qyxy.baic.gov.cn/gjjbj'
@@ -34,5 +36,5 @@ if __name__ == "__main__":
     uri11 ='http://qyxy.baic.gov.cn/gjjbj/gjjQueryCreditAction!openEntInfo.dhtml?entId=308F6BA7DA4D46BF81B02745E0E6B133&credit_ticket=82686F809C80EA54D1DCE356A4FDC88D&entNo=110101600395241&timeStamp=1464141392430'
     uri12='http://qyxy.baic.gov.cn/gjjbj/gjjQueryCreditAction!openInfo.dhtml?entId=308F6BA7DA4D46BF81B02745E0E6B133&entNo=110101600395241&credit_ticket=82686F809C80EA54D1DCE356A4FDC88D&str=2&timeStamp=1464141479578'
     uri13='http://qyxy.baic.gov.cn/gjjbj/gjjQueryCreditAction!openInfo.dhtml?entId=308F6BA7DA4D46BF81B02745E0E6B133&entNo=110101600395241&credit_ticket=82686F809C80EA54D1DCE356A4FDC88D&str=3&timeStamp=1464141513196'
-    uri_list=[uri5]
+    uri_list=[uri22,uri23]
     test_ip_dead(nums,uri_list)
