db26a95af18b468257b019ba33300986229c1577
unknown <>
2016-05-31 15:31:53 +0800

    完善导出层多对一字段的适应

--- clawer/structure/extracters/sql_generator.py
+++ clawer/structure/extracters/sql_generator.py
@@ -54,23 +54,22 @@ class JsonToSql(object):
         """
         tables_info = self.config_dic['table']
         db_name = self.config_dic['database']['destination_db']['dbname']
-        table_sql = open(table_file, 'w')
-        #database_sql = 'DROP DATABASE IF EXISTS %s;\nCREATE DATABASE %s;\nUSE %s;\n' % (db_name, db_name, db_name)
-        database_sql = 'CREATE DATABASE %s;\nUSE %s;\n' % (db_name, db_name)
-        table_sql.write(database_sql.encode('utf8'))
-        sql = ''
-        for t_name in tables_info:
-            table = tables_info[t_name]
-            sql = 'CREATE TABLE %s ( ' % (t_name)
-            for field in table:
-                sql += field['dest_field'] + ' ' + field['datatype']
-                if field['option']:
-                    sql += ' ' + (field['option'])
-                sql += ','
-            sql = sql[0:len(sql) - 1]
-            sql += ');\n\n'
-            table_sql.write(sql.encode('utf8'))
-        table_sql.close()
+        with open(table_file, 'w') as table_sql:
+            database_sql = 'USE %s;\n' % (db_name)
+            table_sql.write(database_sql.encode('utf8'))
+            sql = ''
+            for t_name in tables_info:
+                table = tables_info[t_name]
+                field_dict = {}
+                for field in table:
+                    value = '%s %s' % (field['datatype'], field['option'])
+                    field_dict[field['dest_field']] = value
+
+                sql = 'CREATE TABLE %s (' % (t_name)
+                for k, v in field_dict.items():
+                    sql += '%s %s,' % (k, v)
+                sql = sql[0:len(sql)-1] + ');\n\n'
+                table_sql.write(sql.encode('utf8'))
 
     def create_data_sql(self, data, table_path, associated_field_path):
         """
@@ -82,15 +81,6 @@ class JsonToSql(object):
         """
 
         #*****************************换成字典****
-        '''
-        source = json.loads(data)
-        self.id = source.keys()[0]        # 获取许可证号，待改进
-        if not source[source.keys()[0]]:
-            print source.keys()
-            print 'key'
-            print source[source.keys()[0]]
-            return False
-        '''
         self.id = data.keys()[0]
         if not data[self.id]:
             return False
@@ -153,11 +143,22 @@ class JsonToSql(object):
         values = []    #记录字段的数据
 
         for k in self.mapping[self.table_name]['dest_field'].keys():
-            field.append(self.mapping[self.table_name]['dest_field'][k])
+            mapping_field_value = self.mapping[self.table_name]['dest_field'][k]
+
+            try:
+                #如果field中已经存在该字段，且merged_dict[k]存在，那就改变对应的values的值为merged_dict[k]
+                index = field.index(mapping_field_value)
+                if not values[index] and k in merged_dict.keys():
+                    values[index] = merged_dict[k]
+                continue
+            except ValueError:
+                field.append(mapping_field_value)
+
             try:
                 values.append(merged_dict[k])
             except KeyError:
                 values.append('')    #源数据中没有要找的信息，用空串占位
+
         values = ["'%s'" % i for i in values]
         sql = 'INSERT INTO %s (%s) VALUE(%s); \n' % (self.table_name, ', '.join(field), ', '.join(values))
 
@@ -187,48 +188,32 @@ class JsonToSql(object):
         :param export_file: 输入的sql文件
         :return: True/False
         """
-
-        #tmp = data.decode('utf8')
-        #print type(tmp)
-        #print tmp
-        print 'start get data'
         if not extracter_conf or not data or not export_file:
             return False
-        print 'get data dict'
         if not isinstance(data, types.DictionaryType):
             return False
-        print 'config ok'
         # self.config_dic = self.parser_json(extracter_conf)
         self.config_dic = extracter_conf
         self.get_mapping()
         self.data_sql = open(export_file, 'w')
         use_database = 'use %s;\n' % self.config_dic['database']['destination_db']['dbname']
         self.data_sql.write(use_database)
-        print 'confing........oooo'
         for k in self.mapping.keys():
             self.table_name = self.mapping[k]['dest_table']
             path = self.mapping[k]['source_path']
             associated_field_path = self.mapping[k]['associated_field_source_path']
-            print 'create data sql'
             result = self.create_data_sql(data, path, associated_field_path)
-            print 'over data sql'
-            #if not result:
-            #return False
             self.tmp_dic.clear()
         self.data_sql.close()
-        print 'get data over'
         return True
 
     def test_restore(self, sql_file):
         result = 'mysql %s < %s' % (self.create_commond(), sql_file)
         os.popen(result)
-        #tmp = os.popen(result).readlines()
 
     def test_backup(self, sql_file):
         result = 'mysqldump %s > %s' % (self.create_commond(), sql_file)
         os.popen(result)
-        #tmp = os.popen(result).readlines()
-
 
 if __name__ == '__main__':
     json_to_sql = JsonToSql()
