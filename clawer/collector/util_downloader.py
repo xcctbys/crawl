040d351e7f02b30b2096e6d11a6bd85f54927f29
iygnohz <iygnohz@gmail.com>
2016-06-07 21:31:52 +0800

    Remove rubbish.

--- clawer/collector/util_downloader.py
+++ clawer/collector/util_downloader.py
@@ -35,14 +35,10 @@ class Download(object):
              'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64; rv:39.0) Gecko/20100101 Firefox/39.0'})
 
     def exec_command(self, commandstr, path=None):
-        # print commandstr
         if path:
             sys.path.append(path)
-        # sys.path.append('/Users/princetechs3/my_code')
         c = compile(commandstr, "", 'exec')
         exec c
-        # TODO: Fix it
-        return None
 
     def download_with_enterprise(self):
         print 'i am come in enterprise download'
@@ -50,24 +46,18 @@ class Download(object):
         start_time = time.time()
 
         try:
-            #  uri = 'enterprise://%E9%87%8D%E5%BA%86/%E9%87%8D%E5%BA%86%E7%90%86%E5%BF%85%E6%98%93%E6%8A%95%E8%B5%84%E7%AE%A1%E7%90%86%E6%9C%89%E9%99%90%E5%85%AC%E5%8F%B8/500905004651063/'
             downloader = EnterpriseDownload(self.task.uri)
             list_res = downloader.download()
             list_result = json.loads(list_res)
             end_time = time.time()
             spend_time = end_time - start_time
-            print '----------------json data---------------------'
             for result in list_result:
-                print result
-                print type(result)
                 requests_headers = result.get('requests_headers', 'None')
                 response_headers = result.get('response_headers', 'None')
                 requests_body = result.get('requests_body', 'None')
                 response_body = str(result)
                 remote_ip = result.get('remote_ip', 'None')
                 hostname = str(socket.gethostname())
-
-                # write_downloaddata_to_mongo
                 cdd = CrawlerDownloadData(job=self.task.job,
                                           downloader=self.crawler_download,
                                           crawlertask=self.task,
@@ -78,7 +68,6 @@ class Download(object):
                                           hostname=hostname,
                                           remote_ip=remote_ip)
                 cdd.save()
-                # write_downloaddata_success_log_to_mongo
                 cdl = CrawlerDownloadLog(
                     job=self.task.job,
                     task=self.task,
@@ -160,20 +149,15 @@ class Download(object):
                 result = self.exec_command('import pythoncode%s; result = pythoncode%s.run("%s")' % (
                     str(self.crawler_download.id), str(self.crawler_download.id), self.task.uri),
                                            path=os.path.dirname(filename))
-                # exec 'import pythoncode%s; result = pythoncode%s.run("%s")' % (str(self.crawler_download.id), str(self.crawler_download.id), self.task.uri)
-                # print result
                 end_time = time.time()
 
                 spend_time = end_time - start_time
                 requests_headers = result.get('requests_headers', 'None')
                 response_headers = result.get('response_headers', 'None')
                 requests_body = result.get('requests_body', 'None')
-                # response_body = result.get('response_body', 'None')
                 response_body = str(result)
                 remote_ip = result.get('remote_ip', 'None')
                 hostname = str(socket.gethostname())
-
-                # write_downloaddata_to_mongo
                 cdd = CrawlerDownloadData(job=self.task.job,
                                           downloader=self.crawler_download,
                                           crawlertask=self.task,
@@ -184,7 +168,6 @@ class Download(object):
                                           hostname=hostname,
                                           remote_ip=remote_ip)
                 cdd.save()
-                # write_downloaddata_success_log_to_mongo
                 cdl = CrawlerDownloadLog(
                     job=self.task.job,
                     task=self.task,
@@ -196,20 +179,13 @@ class Download(object):
                     spend_time=spend_time)
                 cdl.save()
 
-                # 改变这个任务的状态为下载成功
                 self.task.status = CrawlerTask.STATUS_SUCCESS
                 self.task.save()
-
-                # os.remove(filename)
-
-            except Exception as e:
-                # 改变这个任务的状态为下载失败
+            except Exception:
                 self.task.status = CrawlerTask.STATUS_FAIL
                 self.task.save()
-
                 end_time = time.time()
                 spend_time = end_time - start_time
-                # write_downloaddata_fail_log_to_mongo
                 cdl = CrawlerDownloadLog(job=self.task.job,
                                          task=self.task,
                                          status=CrawlerDownloadLog.STATUS_FAIL,
@@ -219,40 +195,26 @@ class Download(object):
                                          downloads_hostname=str(socket.gethostname()),
                                          spend_time=spend_time)
                 cdl.save()
-                print e, 'sentry.excepterror()'
                 print "Trackback:", traceback.format_exc()
-                return
-            pass
 
         elif self.crawler_download.types.language == 'shell':
             start_time = time.time()
-            print 'it is shell ---------------------------'
-            # print 'result = commands.getstatusoutput(sh code)'
-            # filename = os.path.join( settings.CODE_PATH, 'shellcode%s.sh' % str(self.crawler_download.id))
             try:
                 filename = os.path.join(settings.CODE_PATH, 'shellcode%s.sh' % str(self.crawler_download.id))
-                # filename = os.path.join( '/Users/princetechs3/my_code', 'pythoncode%s.py' % str(self.crawler_download.id))
                 if not os.path.exists(filename):
                     with open(filename, 'w') as f:
                         f.write(self.crawler_download.code)
-                # os.system("chmod +x %s" % filename)
                 result = commands.getstatusoutput('sh %s %s' % (filename, self.task.uri))
                 result = dict()
-                # result = json.loads(result)
-                # print result
-
                 end_time = time.time()
                 spend_time = end_time - start_time
 
                 requests_headers = result.get('requests_headers', 'None')
                 response_headers = result.get('response_headers', 'None')
                 requests_body = result.get('requests_body', 'None')
-                # response_body = result.get('response_body', 'None')
                 response_body = str(result)
                 remote_ip = result.get('remote_ip', 'None')
                 hostname = str(socket.gethostname())
-
-                # write_downloaddata_to_mongo
                 cdd = CrawlerDownloadData(job=self.task.job,
                                           downloader=self.crawler_download,
                                           crawlertask=self.task,
@@ -263,7 +225,6 @@ class Download(object):
                                           hostname=hostname,
                                           remote_ip=remote_ip)
                 cdd.save()
-                # write_downloaddata_success_log_to_mongo
                 cdl = CrawlerDownloadLog(
                     job=self.task.job,
                     task=self.task,
@@ -274,19 +235,13 @@ class Download(object):
                     downloads_hostname=hostname,
                     spend_time=spend_time)
                 cdl.save()
-
-                # 改变这个任务的状态为下载成功
                 self.task.status = CrawlerTask.STATUS_SUCCESS
                 self.task.save()
-
-            except Exception as e:
-                # 改变这个任务的状态为下载失败
+            except Exception:
                 self.task.status = CrawlerTask.STATUS_FAIL
                 self.task.save()
-
                 end_time = time.time()
                 spend_time = end_time - start_time
-                # write_downloaddata_fail_log_to_mongo
                 cdl = CrawlerDownloadLog(job=self.task.job,
                                          task=self.task,
                                          status=CrawlerDownloadLog.STATUS_FAIL,
@@ -296,18 +251,13 @@ class Download(object):
                                          downloads_hostname=str(socket.gethostname()),
                                          spend_time=spend_time)
                 cdl.save()
-                print e, 'sentry.excepterror()'
                 print "Trackback:", traceback.format_exc()
             pass
 
         elif self.crawler_download.types.language == 'curl':
             start_time = time.time()
-            print 'it is curl ----------------------------'
             try:
-
                 result = commands.getstatusoutput('curl %s' % self.task.uri)
-                # print result
-
                 end_time = time.time()
                 spend_time = end_time - start_time
 
@@ -317,8 +267,6 @@ class Download(object):
                 response_body = str(result)
                 remote_ip = 'None'
                 hostname = str(socket.gethostname())
-
-                # write_downloaddata_to_mongo
                 cdd = CrawlerDownloadData(job=self.task.job,
                                           downloader=self.crawler_download,
                                           crawlertask=self.task,
@@ -329,7 +277,6 @@ class Download(object):
                                           hostname=hostname,
                                           remote_ip=remote_ip)
                 cdd.save()
-                # write_downloaddata_success_log_to_mongo
                 cdl = CrawlerDownloadLog(
                     job=self.task.job,
                     task=self.task,
@@ -340,19 +287,14 @@ class Download(object):
                     downloads_hostname=hostname,
                     spend_time=spend_time)
                 cdl.save()
-
-                # 改变这个任务的状态为下载成功
                 self.task.status = CrawlerTask.STATUS_SUCCESS
                 self.task.save()
-
-            except Exception as e:
-                # 改变这个任务的状态为下载失败
+            except Exception:
                 self.task.status = CrawlerTask.STATUS_FAIL
                 self.task.save()
 
                 end_time = time.time()
                 spend_time = end_time - start_time
-                # write_downloaddata_fail_log_to_mongo
                 cdl = CrawlerDownloadLog(job=self.task.job,
                                          task=self.task,
                                          status=CrawlerDownloadLog.STATUS_FAIL,
@@ -362,17 +304,12 @@ class Download(object):
                                          downloads_hostname=str(socket.gethostname()),
                                          spend_time=spend_time)
                 cdl.save()
-                print e, 'sentry.excepterror()'
                 print "Trackback:", traceback.format_exc()
             pass
         else:
             start_time = time.time()
             try:
                 resp = self.reqst.get(self.task.uri, timeout=25)
-                # print resp.headers
-                # print resp.request.headers
-                # print resp.text
-
                 end_time = time.time()
                 spend_time = end_time - start_time
 
@@ -383,8 +320,6 @@ class Download(object):
                 response_body = unicode(resp.text)
                 remote_ip = resp.headers.get('remote_ip', 'None')
                 hostname = str(socket.gethostname())
-
-                # write_downloaddata_to_mongo
                 cdd = CrawlerDownloadData(job=self.task.job,
                                           downloader=self.crawler_download,
                                           crawlertask=self.task,
@@ -415,7 +350,6 @@ class Download(object):
                     cdd.files_down.put(resp.text, content_type='image/png')
 
                 cdd.save()
-                # write_downloaddata_success_log_to_mongo
                 cdl = CrawlerDownloadLog(
                     job=self.task.job,
                     task=self.task,
@@ -426,19 +360,14 @@ class Download(object):
                     downloads_hostname=hostname,
                     spend_time=spend_time)
                 cdl.save()
-
-                # 改变这个任务的状态为下载成功
                 self.task.status = CrawlerTask.STATUS_SUCCESS
                 self.task.save()
 
-            except Exception as e:
-                # 改变这个任务的状态为下载失败
+            except Exception:
                 self.task.status = CrawlerTask.STATUS_FAIL
                 self.task.save()
-
                 end_time = time.time()
                 spend_time = end_time - start_time
-                # write_downloaddata_fail_log_to_mongo
                 cdl = CrawlerDownloadLog(job=self.task.job,
                                          task=self.task,
                                          status=CrawlerDownloadLog.STATUS_FAIL,
@@ -448,20 +377,16 @@ class Download(object):
                                          downloads_hostname=str(socket.gethostname()),
                                          spend_time=spend_time)
                 cdl.save()
-                print e, 'sentry.excepterror()'
                 print "Trackback:", traceback.format_exc()
 
 
 def force_exit(download_timeout, task):
     """
-		通过定时器触发此函数，强制退出运行父进程，并将子进程杀死。
-	"""
+    通过定时器触发此函数，强制退出运行父进程，并将子进程杀死。
+    """
     pgid = os.getpgid(0)
-    # 改变这个任务的状态为下载失败
     task.status = CrawlerTask.STATUS_FAIL
     task.save()
-
-    # write_downloaddata_fail_log_to_mongo
     cdl = CrawlerDownloadLog(job=task.job,
                              task=task,
                              status=CrawlerDownloadLog.STATUS_FAIL,
@@ -472,22 +397,18 @@ def force_exit(download_timeout, task):
                              spend_time=download_timeout)
     cdl.save()
 
-    os.killpg(pgid, 9)    #这行代码不能够删除
+    os.killpg(pgid, 9)    # 这行代码不能够删除
     os._exit(1)
 
 
 def download_clawer_task(task):
-    #加载对应job的设置任务
-    print '----------------------come in------------------------------'
+    # 加载对应job的设置任务
     try:
         crawler_download = CrawlerDownload.objects(job=task.job).first()
-        # print crawler_download.code,crawler_download.types.language
         crawler_download_setting = CrawlerDownloadSetting.objects(job=task.job).first()
-        # print crawler_download_setting
-    except Exception as e:
+    except Exception:
         task.status = CrawlerTask.STATUS_FAIL
         task.save()
-        print e, 'sentry.excepterror()'
     down = Download(task, crawler_download, crawler_download_setting)
 
     timer = threading.Timer(crawler_download_setting.download_timeout, force_exit,
